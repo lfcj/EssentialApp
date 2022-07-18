b1c92927902cf4b02fe047e4a70e92698383e8df
lfcj <fernanda16400@gmail.com>
2022-07-18 09:03:07 +0200

    Extract `FeedLoader` `expect` helper into a shared extension to make it reusable

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -52,25 +52,4 @@ private extension FeedLoaderWithFallbackCompositeTests {
         return sut
     }
 
-    func expect(
-        _ sut: FeedLoaderWithFallbackComposite,
-        toCompleteWith expectedResult: FeedLoader.Result,
-        file: StaticString = #file,
-        line: UInt = #line
-    ) {
-        let exp = expectation(description: "Expect remote feed")
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case (.success(let receivedFeed), .success(let expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-            case (.failure(let receivedError), .failure(let expectedError)):
-                XCTAssertEqual(receivedError as NSError, expectedError as NSError, file: file, line: line)
-            default:
-                XCTFail("Expected \(expectedResult) and received \(receivedResult)", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-    }
-
 }
