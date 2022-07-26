6b654fc2023aa5eacdf388fb9ab9cdf705aca803
Luisa Castano <lcastano@kayak.com>
2022-07-26 23:06:56 +0200

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness – We can now run UI tests without network connection

--- EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
+++ EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
@@ -6,15 +6,60 @@ final class DebuggingSceneDelegate: SceneDelegate {
 
     // MARK: - Nested Types
 
-    private class AlwaysFailingHTTPClient: HTTPClient {
+    private class DebuggingHTTPClient: HTTPClient {
         private class Task: HTTPClientTask {
             func cancel() {}
         }
+        private let connectivity: String
+        init(connectivity: String) {
+            self.connectivity = connectivity
+        }
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            let error = NSError(domain: "offline", code: -1)
-            completion(.failure(error))
+            switch connectivity {
+            case "online":
+                completion(.success(makeSuccessfulResponse(for: url)))
+            default:
+                completion(.failure(NSError(domain: "offline", code: -1)))
+            }
             return Task()
         }
+
+        private func makeSuccessfulResponse(for url: URL) -> (Data, HTTPURLResponse) {
+            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            return (makeData(for: url), response)
+        }
+
+        private func makeData(for url: URL) -> Data {
+            switch url.absoluteString {
+            case "http://image.com":
+                return makeImageData()
+            default:
+                return makeFeedData()
+            }
+        }
+
+        private func makeImageData() -> Data {
+            let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+            UIGraphicsBeginImageContext(rect.size)
+            let context = UIGraphicsGetCurrentContext()
+            context?.setFillColor(UIColor.red.cgColor)
+            context?.fill(rect)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image!.pngData()!
+        }
+
+        private func makeFeedData() -> Data {
+            try! JSONSerialization.data(
+                withJSONObject: [
+                    "items": [
+                        ["id": UUID().uuidString, "image": "http://image.com"],
+                        ["id": UUID().uuidString, "image": "http://image.com"]
+                    ]
+                ]
+            )
+        }
+
     }
 
     override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -30,8 +75,8 @@ final class DebuggingSceneDelegate: SceneDelegate {
     }
 
     override func makeHTTPClient() -> HTTPClient {
-        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-            return AlwaysFailingHTTPClient()
+        if let connectivity = UserDefaults.standard.string(forKey: "connectivity") {
+            return DebuggingHTTPClient(connectivity: connectivity)
         }
         return super.makeHTTPClient()
     }
