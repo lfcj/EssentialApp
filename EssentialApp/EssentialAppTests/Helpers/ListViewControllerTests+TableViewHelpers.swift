a0028dd12bdd30dc03a6e158b3dcfcb6fb3348ee
Luisa Castano <lcastano@kayak.com>
2022-10-30 22:11:52 +0100

    Load more on error tap

--- EssentialApp/EssentialAppTests/Helpers/ListViewControllerTests+TableViewHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewControllerTests+TableViewHelpers.swift
@@ -109,6 +109,12 @@ extension ListViewController {
         dataSource?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
+    func simulateTapOnLoadMoreFeedError() {
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, didSelectRowAt: index)
+    }
+
     private func loadMoreFeedCell() -> LoadMoreCell? {
         cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
